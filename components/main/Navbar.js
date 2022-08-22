c94be15ed64128c8f7275ddf3cfb3a3263b28726
aliff <alifzulkifeli@gmail.com>
2022-08-23 00:07:10 +0800

    Add layout

--- components/main/Navbar.js
+++ components/main/Navbar.js
@@ -14,56 +14,56 @@ const Navbar = () => {
   const offsetMobile = -378
 
   return (
-    <nav class="bg-white shadow-lg sticky top-0">
-      <div class="max-w-6xl mx-auto px-4">
-        <div class="flex justify-between">
-          <div class="flex space-x-7">
+    <nav className="bg-white shadow-lg sticky top-0">
+      <div className="max-w-6xl mx-auto px-4">
+        <div className="flex justify-between">
+          <div className="flex space-x-7">
             <div>
 
-              <a href="#" class="flex items-center py-4 px-2">
-                <img src="logo.jpg" alt="Logo" class="h-8 w-8 mr-2" />
-                <span class="font-semibold text-gray-500 text-lg">Wani's Page</span>
+              <a href="#" className="flex items-center py-4 px-2">
+                <img src="logo.jpg" alt="Logo" className="h-8 w-8 mr-2" />
+                <span className="font-semibold text-gray-500 text-lg">Wani's Page</span>
               </a>
             </div>
 
-            <div class="hidden md:flex items-center space-x-1">
+            <div className="hidden md:flex items-center space-x-1">
 
-              <Link activeClass={activeNavbar} className={defaultNavbar} smooth spy to="home" offset={offset}>
+              <Link activeclassName={activeNavbar} className={defaultNavbar} smooth spy to="home" offset={offset}>
                 Home
               </Link>
 
-              <Link activeClass={activeNavbar} className={defaultNavbar} smooth spy to="medical-card" offset={offset}>
+              <Link activeclassName={activeNavbar} className={defaultNavbar} smooth spy to="medical-card" offset={offset}>
                 Medical Card
               </Link>
 
-              <Link activeClass={activeNavbar} className={defaultNavbar} smooth spy to="hibah" offset={offset}>
+              <Link activeclassName={activeNavbar} className={defaultNavbar} smooth spy to="hibah" offset={offset}>
                 Hibah
               </Link>
 
 
-              <Link activeClass={activeNavbar} className={defaultNavbar} smooth spy to="quotation" offset={offset}>
+              <Link activeclassName={activeNavbar} className={defaultNavbar} smooth spy to="quotation" offset={offset}>
                 Quotation
               </Link>
 
-              <Link activeClass={activeNavbar} className={defaultNavbar} smooth spy to="contact" offset={offset}>
+              <Link activeclassName={activeNavbar} className={defaultNavbar} smooth spy to="contact" offset={offset}>
                 Contact
               </Link>
 
-              <Link activeClass={activeNavbar} className={defaultNavbar} smooth spy to="gallery" offset={offset}>
+              <Link activeclassName={activeNavbar} className={defaultNavbar} smooth spy to="gallery" offset={offset}>
                 Gallery
               </Link>
             </div>
           </div>
 
           {/* put whatsapp here */}
-          {/* <div class="hidden md:flex items-center space-x-3 ">
-						<a href="" class="py-2 px-2 font-medium text-gray-500 rounded hover:bg-green-500 hover:text-white transition duration-300">Log In</a>
-						<a href="" class="py-2 px-2 font-medium text-white bg-green-500 rounded hover:bg-green-400 transition duration-300">Sign Up</a>
+          {/* <div className="hidden md:flex items-center space-x-3 ">
+						<a href="" className="py-2 px-2 font-medium text-gray-500 rounded hover:bg-green-500 hover:text-white transition duration-300">Log In</a>
+						<a href="" className="py-2 px-2 font-medium text-white bg-green-500 rounded hover:bg-green-400 transition duration-300">Sign Up</a>
 					</div> */}
 
-          <div class="md:hidden flex items-center">
-            <button onClick={() => settoggle(!toggle)} class="outline-none mobile-menu-butto ">
-              <svg class=" w-6 h-6 text-gray-500 hover:text-green-500 "
+          <div className="md:hidden flex items-center">
+            <button onClick={() => settoggle(!toggle)} className="outline-none mobile-menu-butto ">
+              <svg className=" w-6 h-6 text-gray-500 hover:text-green-500 "
                 x-show="!showMenu"
                 fill="none"
                 stroke-linecap="round"
@@ -81,30 +81,30 @@ const Navbar = () => {
 
       <div className={toggle ? "" : "hidden"} >
         <div className=" mobile-menu float">
-          <ul class="">
+          <ul className="">
 
 
-            <Link activeClass={activeNavbarMobile} className={defaultNavbarMobile} smooth spy to="home" offset={offsetMobile} onClick={() => settoggle(!toggle)}>
+            <Link activeclassName={activeNavbarMobile} className={defaultNavbarMobile} smooth spy to="home" offset={offsetMobile} onClick={() => settoggle(!toggle)}>
               Home
             </Link>
 
-            <Link activeClass={activeNavbarMobile} className={defaultNavbarMobile} smooth spy to="medical-card" offset={offsetMobile} onClick={() => settoggle(!toggle)}>
+            <Link activeclassName={activeNavbarMobile} className={defaultNavbarMobile} smooth spy to="medical-card" offset={offsetMobile} onClick={() => settoggle(!toggle)}>
               Medical Card
             </Link>
 
-            <Link activeClass={activeNavbarMobile} className={defaultNavbarMobile} smooth spy to="hibah" offset={offsetMobile} onClick={() => settoggle(!toggle)}>
+            <Link activeclassName={activeNavbarMobile} className={defaultNavbarMobile} smooth spy to="hibah" offset={offsetMobile} onClick={() => settoggle(!toggle)}>
               Hibah
             </Link>
 
-            <Link activeClass={activeNavbarMobile} className={defaultNavbarMobile} smooth spy to="quotation" offset={offsetMobile} onClick={() => settoggle(!toggle)}>
+            <Link activeclassName={activeNavbarMobile} className={defaultNavbarMobile} smooth spy to="quotation" offset={offsetMobile} onClick={() => settoggle(!toggle)}>
               Quotation
             </Link>
 
-            <Link activeClass={activeNavbarMobile} className={defaultNavbarMobile} smooth spy to="contact" offset={offsetMobile} onClick={() => settoggle(!toggle)}>
+            <Link activeclassName={activeNavbarMobile} className={defaultNavbarMobile} smooth spy to="contact" offset={offsetMobile} onClick={() => settoggle(!toggle)}>
               Contact
             </Link>
 
-            <Link activeClass={activeNavbarMobile} className={defaultNavbarMobile} smooth spy to="gallery" offset={offsetMobile} onClick={() => settoggle(!toggle)}>
+            <Link activeclassName={activeNavbarMobile} className={defaultNavbarMobile} smooth spy to="gallery" offset={offsetMobile} onClick={() => settoggle(!toggle)}>
               Gallery
             </Link>
 
