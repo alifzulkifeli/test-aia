feaa0af2a0f937c6ed58bdf54ec1f9b8c97f247c
alifzulkifeli <alifzulkifeli@gmail.com>
2022-08-23 20:37:35 +0800

    layou

--- components/main/Navbar.js
+++ components/main/Navbar.js
@@ -1,16 +1,18 @@
 import { useState } from "react";
 import { Link } from "react-scroll";
+import { IoLogoWhatsapp } from "react-icons/io5";
 
 const Navbar = () => {
 
   const [toggle, settoggle] = useState(false);
   const [highlighted, sethighlighted] = useState("")
 
-  const defaultNavbar = "py-4 px-2 text-gray-500 font-semibold hover:text-green-500 transition duration-300"
+  const defaultNavbar = "py-4 px-2 text-gray-500 font-semibold hover:text-green-500 hover:border-b-4 hover:border-green-500 transition duration-300"
   const activeNavbar = "py-4 px-2 text-green-500 border-b-4 border-green-500 font-semibold"
-  const offset = -65
   const defaultNavbarMobile = "block text-sm px-2 py-4 hover:bg-green-500 transition duration-300"
   const activeNavbarMobile = "block text-sm px-2 py-4 text-white bg-green-500 font-semibold"
+
+  const offset = -65
   const offsetMobile = -378
 
   return (
@@ -22,52 +24,51 @@ const Navbar = () => {
 
               <a href="#" className="flex items-center py-4 px-2">
                 <img src="logo.jpg" alt="Logo" className="h-8 w-8 mr-2" />
-                <span className="font-semibold text-gray-500 text-lg">Main Page</span>
+                <span className="font-semibold text-gray-500 text-lg">Wani&#39;s Page</span>
               </a>
             </div>
 
             <div className="hidden md:flex items-center space-x-1">
 
-              <Link activeclassName={activeNavbar} className={defaultNavbar} smooth spy to="home" offset={offset}>
+              <Link activeClass={activeNavbar} className={defaultNavbar} smooth spy to="home" offset={offset}>
                 Home
               </Link>
 
-              <Link activeclassName={activeNavbar} className={defaultNavbar} smooth spy to="medical-card" offset={offset}>
+              <Link activeClass={activeNavbar}  className={defaultNavbar} smooth spy to="medical-card" offset={offset}>
                 Medical Card
               </Link>
 
-              <Link activeclassName={activeNavbar} className={defaultNavbar} smooth spy to="hibah" offset={offset}>
+              <Link activeClass={activeNavbar}  className={defaultNavbar} smooth spy to="hibah" offset={offset}>
                 Hibah
               </Link>
 
 
-              <Link activeclassName={activeNavbar} className={defaultNavbar} smooth spy to="quotation" offset={offset}>
+              <Link activeClass={activeNavbar}  className={defaultNavbar} smooth spy to="quotation" offset={offset}>
                 Quotation
               </Link>
 
-              <Link activeclassName={activeNavbar} className={defaultNavbar} smooth spy to="contact" offset={offset}>
+              <Link activeClass={activeNavbar}  className={defaultNavbar} smooth spy to="contact" offset={offset}>
                 Contact
               </Link>
 
-              <Link activeclassName={activeNavbar} className={defaultNavbar} smooth spy to="gallery" offset={offset}>
+              <Link activeClass={activeNavbar}  className={defaultNavbar} smooth spy to="gallery" offset={offset}>
                 Gallery
               </Link>
             </div>
           </div>
 
           {/* put whatsapp here */}
-          {/* <div className="hidden md:flex items-center space-x-3 ">
-						<a href="" className="py-2 px-2 font-medium text-gray-500 rounded hover:bg-green-500 hover:text-white transition duration-300">Log In</a>
-						<a href="" className="py-2 px-2 font-medium text-white bg-green-500 rounded hover:bg-green-400 transition duration-300">Sign Up</a>
-					</div> */}
+          <div className="flex items-center  md:space-x-3 ">
+            <a href="https://wa.link/38ylx6" className="py-2 px-2 font-medium text-white bg-green-500 rounded hover:bg-green-400 transition duration-300 flex">Chat Us <IoLogoWhatsapp className=" ml-1" size={23} /> </a>
+          </div>
 
           <div className="md:hidden flex items-center">
             <button onClick={() => settoggle(!toggle)} className="outline-none mobile-menu-butto ">
               <svg className=" w-6 h-6 text-gray-500 hover:text-green-500 "
                 x-show="!showMenu"
                 fill="none"
-                strokeLineCap="round"
-                strokeLineJoin="round"
+                strokeLinecap="round"
+                strokeLinejoin="round"
                 strokeWidth="2"
                 viewBox="0 0 24 24"
                 stroke="currentColor"
@@ -84,27 +85,27 @@ const Navbar = () => {
           <ul className="">
 
 
-            <Link activeclassName={activeNavbarMobile} className={defaultNavbarMobile} smooth spy to="home" offset={offsetMobile} onClick={() => settoggle(!toggle)}>
+            <Link activeClass={activeNavbarMobile} className={defaultNavbarMobile} smooth spy to="home" offset={offsetMobile} onClick={() => settoggle(!toggle)}>
               Home
             </Link>
 
-            <Link activeclassName={activeNavbarMobile} className={defaultNavbarMobile} smooth spy to="medical-card" offset={offsetMobile} onClick={() => settoggle(!toggle)}>
+            <Link activeClass={activeNavbarMobile} className={defaultNavbarMobile} smooth spy to="medical-card" offset={offsetMobile} onClick={() => settoggle(!toggle)}>
               Medical Card
             </Link>
 
-            <Link activeclassName={activeNavbarMobile} className={defaultNavbarMobile} smooth spy to="hibah" offset={offsetMobile} onClick={() => settoggle(!toggle)}>
+            <Link activeClass={activeNavbarMobile} className={defaultNavbarMobile} smooth spy to="hibah" offset={offsetMobile} onClick={() => settoggle(!toggle)}>
               Hibah
             </Link>
 
-            <Link activeclassName={activeNavbarMobile} className={defaultNavbarMobile} smooth spy to="quotation" offset={offsetMobile} onClick={() => settoggle(!toggle)}>
+            <Link activeClass={activeNavbarMobile} className={defaultNavbarMobile} smooth spy to="quotation" offset={offsetMobile} onClick={() => settoggle(!toggle)}>
               Quotation
             </Link>
 
-            <Link activeclassName={activeNavbarMobile} className={defaultNavbarMobile} smooth spy to="contact" offset={offsetMobile} onClick={() => settoggle(!toggle)}>
+            <Link activeClass={activeNavbarMobile} className={defaultNavbarMobile} smooth spy to="contact" offset={offsetMobile} onClick={() => settoggle(!toggle)}>
               Contact
             </Link>
 
-            <Link activeclassName={activeNavbarMobile} className={defaultNavbarMobile} smooth spy to="gallery" offset={offsetMobile} onClick={() => settoggle(!toggle)}>
+            <Link activeClass={activeNavbarMobile} className={defaultNavbarMobile} smooth spy to="gallery" offset={offsetMobile} onClick={() => settoggle(!toggle)}>
               Gallery
             </Link>
 
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import Navbar from '../components/main/Navbar';
 import Header from '../components/main/Header';
+import Footer from './../components/main/Footer';
 
 
 export default function Home() {
@@ -9,12 +10,12 @@ export default function Home() {
     <div>
       <Navbar/>
       <section id="home"> <Header /></section>
-      <section id="medical-card"> <hr/> <div className=' min-h-screen ' ></div></section>
-      <section id="hibah"> <hr/> <div className=' min-h-screen ' ></div> </section>
-      <section id="quotation"> <hr/> <div className=' min-h-screen ' ></div> </section>
-      <section id="contact"> <hr/> <div className=' min-h-screen ' ></div> </section>
-      <section id="gallery"> <Header /><Header /><Header /></section>
-      <hr/>
+      <section id="medical-card">  <div className=' min-h-screen border-4 border-sky-500 text-center grid content-center' > <p className=' text-4xl font-bold ' >Medical Card Section</p> </div></section>
+      <section id="hibah">   <div className=' min-h-screen border-4 border-sky-500 text-center grid content-center' > <p className=' text-4xl font-bold ' >Hibah Section</p> </div></section>
+      <section id="quotation">  <div className=' min-h-screen border-4 border-sky-500 text-center grid content-center' > <p className=' text-4xl font-bold ' >Quotation Section</p> </div></section>
+      <section id="contact">  <div className=' min-h-screen border-4 border-sky-500 text-center grid content-center' > <p className=' text-4xl font-bold ' >Contact Section</p> </div></section>
+      <section id="gallery">  <div className=' min-h-screen border-4 border-sky-500 text-center grid content-center' > <p className=' text-4xl font-bold ' >Gallery Section</p> </div></section>
+      <Footer/>
     </div>
   )
 }
